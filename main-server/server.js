e83196be989991449f1c8d889dc3dae198a1d3b9
Shevtsov <shevtsov0053@gmail.com>
2020-02-03 11:59:31 +0300

    Simple servers initialized.

---
+++ main-server/server.js
@@ -0,0 +1,70 @@
+//@ts-check
+var express = require('express');
+var log = require('morgan')('dev');
+var bodyParser = require('body-parser');
+const axios = require('axios').default;
+
+require('dotenv').config() 
+const { PORT, 
+  SERVER_1_URL, SERVER_1_PORT,
+  SERVER_2_URL, SERVER_2_PORT, 
+  SERVER_3_URL, SERVER_3_PORT,
+  SERVER_4_URL, SERVER_4_PORT } = process.env
+if (!PORT) {
+  console.error('!!!PORT IS UNDEFINED!!!')
+  process.exit()
+}
+
+
+//hero routes
+var app = express();
+
+//configure bodyparser
+var bodyParserJSON = bodyParser.json();
+var bodyParserURLEncoded = bodyParser.urlencoded({extended:true});
+
+//initialise express router
+var router = express.Router();
+
+// call the database connectivity function
+
+// configure app.use()
+app.use(log);
+app.use(bodyParserJSON);
+app.use(bodyParserURLEncoded);
+
+// Error handling
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+     res.setHeader("Access-Control-Allow-Credentials", "true");
+     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
+   next();
+ });
+
+// use express router
+// app.use('/api',router);
+
+
+const checkServer = async (serverUrl, serverPort) => {
+  try {
+    const response = await axios.post(`http://${serverUrl}:${serverPort}/check`)
+    const serverAvailable = response.data.message === 'ok'
+    return true
+  } catch(e) {
+    return false
+  }
+}
+
+
+app.post("/send", async (req, res) => {
+  res.json({
+    server1available: await checkServer(SERVER_1_URL, SERVER_1_PORT),
+    server2Available: await checkServer(SERVER_2_URL, SERVER_2_PORT),
+    server3available: await checkServer(SERVER_3_URL, SERVER_3_PORT),
+    server4Available: await checkServer(SERVER_4_URL, SERVER_4_PORT)
+  })
+})
+app.listen(PORT, (req, res) => {
+    console.log(`Product server is running on ${PORT} port.`);
+})
